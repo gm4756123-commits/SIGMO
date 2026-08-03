b7f2b74bf3dffc200a9df04f396a7cca042e2d02
gm4756123-commits <gm4756123@gmail.com>
2026-08-03 06:06:13 +0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -94,7 +94,7 @@ export const botConfig = {
     colors: {
       // Main brand colors.
       primary: "#eb466d",
-      secondary: "#2F3136",
+      secondary: "#e23a64",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
